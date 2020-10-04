28eaf2abbf8089680f2c033ad5a161e5fd90e462
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-10-04 06:41:21 +0600

    volunteer added

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const bodyParser = require("body-parser");
 
 // importing routes
 const event = require("./routes/event");
-
+const volunteer = require("./routes/volunteer");
 // body parser
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use("/api/events", event);
+app.use("/api/volunteers", volunteer);
 
 app.listen(port, () => {
   console.log("server started on port", port);
---
+++ routes/volunteer.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// getting all volunteers
+router.get("/", function (req, res) {
+  console.log("request successful");
+  res.send("success");
+});
+
+// adding a volunteer
+router.post("/add", function (req, res) {
+  const { title, date, description, image } = req.body;
+  console.log(title, date, description, image);
+  res.send("one event added");
+});
+
+// adding multiple events
+
+router.post("/addmany", function (req, res) {});
+
+// deleting a volunteer
+router.delete("/delete/:id", function (req, res) {});
+
+module.exports = router;
