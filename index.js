3057304eaec70a3a73995e42704ee0136ae1a1fc
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-10-04 10:28:07 +0600

    event apis added

--- index.js
+++ index.js
@@ -2,17 +2,130 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 8000;
 const bodyParser = require("body-parser");
-
+const MongoClient = require("mongodb").MongoClient;
 // importing routes
 const event = require("./routes/event");
 const volunteer = require("./routes/volunteer");
 // body parser
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+require("dotenv").config();
 
-app.use("/api/events", event);
-app.use("/api/volunteers", volunteer);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cd3jp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+client.connect(async (err) => {
+  try {
+    const eventCollection = await client
+      .db("volunteer-network")
+      .collection("events");
+    // getting all events
+    // console.log(collection);
+    console.log("connected");
+
+    app.get("/api/events/", async function (req, res) {
+      // console.log("request successful");
+      console.info("request received");
+      try {
+        const result = await eventCollection.find({}).toArray();
+        if (result) {
+          res.send(result);
+          console.log("request successful");
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        client.close();
+      }
+      // console.log(result);
+    });
+
+    // adding a event
+    app.post("/api/events/add", async function (req, res) {
+      console.log("request received");
+
+      const event = req.body;
+      // console.log(title, date, description, image);
+      try {
+        const response = await eventCollection.insertOne(event);
+        if (response) {
+          res.send(response.insertedCount > 0);
+          console.log("request successful");
+        }
+      } catch (err) {
+        console.log("this is an error");
+        console.log(err);
+      } finally {
+        client.close();
+        console.log("closed");
+      }
+      // res.send("one event added");
+    });
+
+    // adding multiple events
+
+    app.post("/api/events/addmany", function (req, res) {});
+  } catch (error) {
+    console.log(error);
+  }
+
+  //volunteers api Collection
+
+  try {
+    const volunteerCollection = await client
+      .db("volunteer-network")
+      .collection("volunteers");
+    // getting all events
+    // console.log(collection);
+    console.log("connected");
+
+    // getting all volunteers
+    app.get("/api/volunteers", async function (req, res) {
+      console.log("request successful");
+      try {
+        const response = await volunteerCollection.find({}).toArray();
+        if (response) {
+          res.send(response);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        client.close();
+      }
+    });
+
+    // adding a volunteer
+    app.post("/api/volunteers/add", async function (req, res) {
+      const volunteer = req.body;
+      // console.log(title, date, description, image);
+      try {
+        const response = await volunteerCollection.insertOne(volunteer);
+        if (response.insertedCount>0) {
+          res.send("one item added successfully");
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        client.close();
+      }
+    });
+
+    // adding multiple events
+
+    app.post("/api/volunteers/addmany", function (req, res) {});
+
+    // deleting a volunteer
+    app.delete("/api/volunteers/delete/:id", function (req, res) {});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// app.use("/api/events", event);
+// app.use("/api/volunteers", volunteer);
 app.listen(port, () => {
   console.log("server started on port", port);
 });
--- routes/event.js
+++ routes/event.js
@@ -1,21 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
 
-// getting all events
-router.get("/", function (req, res) {
-  console.log("request successful");
-  res.send("success");
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cd3jp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+// let collection;
+// client.connect(async (err) => {
+//   try {
+//     collection = await client.db("volunteer-network").collection("events");
+//     // getting all events
+//     console.log(collection);
+//     console.log("connected");
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     client.close();
+//   }
+// });
 
-// adding a event
-router.post("/addone", function (req, res) {
-  const { title, date, description, image } = req.body;
-  console.log(title, date, description, image);
-  res.send("one event added");
-});
+// router.get("/", function (req, res) {
+//   console.log("request successful");
+//   const result = collection.find().toArray();
+//   console.log(result);
+//   res.send(result);
+// });
 
-// adding multiple events
+// // adding a event
+// router.post("/add", function (req, res) {
+//   const { title, date, description, image } = req.body;
+//   console.log(title, date, description, image);
+//   res.send("one event added");
+// });
 
-router.post("/addmany", function (req, res) {});
+// // adding multiple events
+
+// router.post("/addmany", function (req, res) {});
 
 module.exports = router;
